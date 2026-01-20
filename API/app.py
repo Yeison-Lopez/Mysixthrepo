b8a0e0a4eb517df85ddc1cc09c8eccfcef5cb50d
testu4546 <testinguser4546@gmail.com>
2026-01-19 20:02:17 -0500

    Update app.py

--- API/app.py
+++ API/app.py
@@ -34,3 +34,8 @@ def get_starwars_warriors():
 def get_warriors_got():
     rows = ["Jon Snow", "Daenerys Targaryen", "Arya Stark", "Tyrion Lannister", "Cersei Lannister", "Jaime Lannister", "Bran Stark", "Sansa Stark"]
     return rows
+
+@app.get("/superheroesBogota")
+def get_superheroes_marvel():
+    rows = ["Ñero-Man", "Bosa Man", "SoachaMan"]
+    return rows
